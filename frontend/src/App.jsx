e5e367b267b491d87f48b6f358affe0360dee6bd
kshipraDewat <kshipradewat66@gmail.com>
2024-11-13 15:43:33 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+   <div className='p-5'>
+     <Button>sign up</Button>
+   </div>
+  )
+}
+
+export default App
