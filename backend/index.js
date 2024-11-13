7855574008a2bad9bd9b2695dd94b56c41863794
kshipraDewat <kshipradewat66@gmail.com>
2024-11-13 15:29:08 +0530

    application api

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ dotenv.config({});
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js"
 import jobRoute from "./routes/jobs.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 const PORT =process.env.PORT || 3000;
 const app = express();
@@ -26,6 +27,7 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute )
 app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(PORT,()=>{
     connectDB()
