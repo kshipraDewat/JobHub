3f3dbe9f60b911de6d3ae11a2ce595755a9e0a7e
kshipraDewat <kshipradewat66@gmail.com>
2024-11-20 16:34:17 +0530

    Signup page

--- frontend/src/components/Auth/Signup.jsx
+++ frontend/src/components/Auth/Signup.jsx
@@ -1,13 +1,57 @@
 import React from 'react'
 import Navbar from '../ui/shared/Navbar'
+import { Input } from '../ui/input'
+import { Label } from "@/components/ui/label"
+import { RadioGroup } from "@/components/ui/radio-group"
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
-  return (
-    <div>
-      <Navbar/>
-      signup
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <div className='flex items-center justify-center max-w-7xl mx-auto '>
+                <form action="" className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+                    <h1 className='font-bold text-xl mb-5'>Signup</h1>
+                    <div className='my-2'>
+                        <Label>Full Name</Label>
+                        <Input type="text" placeholder=" Eg :- John Doe" />
+                    </div>
+                    <div className='my-2'>
+                        <Label>Email</Label>
+                        <Input type="email" placeholder=" Eg :- JohnDoe@gmail.com" />
+                    </div>
+                    <div className='my-2'>
+                        <Label>Phone Number</Label>
+                        <Input type="text" placeholder=" Eg :- 1234567890" />
+                    </div>
+                    <div className='my-2'>
+                        <Label>Password</Label>
+                        <Input type="password" placeholder="Eg :- ****123 " />
+                    </div>
+                    <div className='flex items-start lg:items-center justify-between  flex-col lg:flex-row'>
+                        <RadioGroup className='flex items-center gap-4 my-5'>
+                            <div className="flex items-center space-x-2">
+                                <Input type= "radio" name="role" value="student" className="cursor-pointer" />
+                                <Label htmlFor="student">Student</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                            <Input type= "radio" name="role" value="recruiter" className="cursor-pointer" />
+                                <Label htmlFor="recruiter">Recruiter</Label>
+                            </div>
+                        </RadioGroup>
+                        <div className='flex items-center gap-2 mb-5 lg:my-5 '>
+                            <Label>Profile</Label>
+                            <Input accept ="image/*" type="file" className="cursor-pointer"/>
+
+                        </div>
+                    </div>
+                    <Button type="submit" className="w-full my-4">Signup</Button>
+                    <span> Already have an account? <Link to='/login'>Login</Link> </span>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Signup
--- frontend/src/components/ui/shared/Navbar.jsx
+++ frontend/src/components/ui/shared/Navbar.jsx
@@ -6,10 +6,11 @@ import {
 import React from 'react'
 import { Button } from '../button'
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
-import { Link, LogOut, User2 } from "lucide-react"
+import { LogOut, User2 } from "lucide-react"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
-  const user = true
+  const user = false
   return (
     <div>
       <div className='flex items-center justify-between mx-auto max-w-7xl h-12' >
@@ -24,8 +25,8 @@ const Navbar = () => {
           </ul>
           {!user ? (
             <div className='flex items-center gap-2'>
-              <Button variant="outline">Login</Button>
-              <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button>
+            <Link to="/login"><Button variant="outline">Login</Button></Link>
+            <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button></Link>
             </div>
           ) : (
             <Popover>
