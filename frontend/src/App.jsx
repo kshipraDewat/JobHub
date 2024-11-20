910e7d1df2a0372f72df067d1e4b9c800cb41204
kshipraDewat <kshipradewat66@gmail.com>
2024-11-20 15:52:58 +0530

    Routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/ui/shared/Navbar'
+import Login from './components/Auth/Login'
+import Signup from './components/Auth/Signup'
+import Home from './components/Home'
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/signup',
+    element: <Signup/>
+  }
+
+])
 function App() {
 
 
   return (
    <div className='p-5'>
-     <Navbar/>
+     <>
+      <RouterProvider router ={appRouter}/>
+     </>
     
    </div>
   )
---
+++ frontend/src/components/Auth/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../ui/shared/Navbar'
+
+const Signup = () => {
+  return (
+    <div>
+      <Navbar/>
+      signup
+    </div>
+  )
+}
+
+export default Signup
