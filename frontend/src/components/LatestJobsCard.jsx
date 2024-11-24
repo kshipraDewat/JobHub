3cbd309757eefefc2138d9bec6b8af6c3c9db422
kshipraDewat <kshipradewat66@gmail.com>
2024-11-24 13:58:00 +0530

    job api integrated

--- frontend/src/components/LatestJobsCard.jsx
+++ frontend/src/components/LatestJobsCard.jsx
@@ -1,21 +1,23 @@
 import { Badge } from "@/components/ui/badge"
+import { useNavigate } from "react-router-dom"
 
 
 const LatestJobsCard = ({job}) => {
+    const navigate = useNavigate()
   return (
-    <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
+    <div onClick={()=> navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
             <div>
-                <h1 className='font-medium text-lg'>Google</h1>
+                <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
                 <p className='text-sm text-gray-500'>India</p>
             </div>
             <div>
-                <h1 className='font-bold text-lg my-2'>Frontend Developer</h1>
-                <p className='text-sm text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, quae.</p>
+                <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
+                <p className='text-sm text-gray-600'>{job?.description}</p>
             </div>
             <div className='flex items-center gap-2 mt-4'>
-                <Badge className={'text-blue-700 font-bold'} variant="ghost">10 Positions</Badge>
-                <Badge className={'text-[#F83002] font-bold'} variant="ghost">Full Time</Badge>
-                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">10 LPA</Badge>
+                <Badge className={'text-blue-700 font-bold'} variant="ghost">{job?.position} Positions</Badge>
+                <Badge className={'text-[#F83002] font-bold'} variant="ghost">{job?.jobType}</Badge>
+                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">{job?.salary} LPA</Badge>
             </div>
 
         </div>
