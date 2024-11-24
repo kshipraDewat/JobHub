c9ca66380c26ca92a702a3e92c8d881c5c2c6a20
kshipraDewat <kshipradewat66@gmail.com>
2024-11-24 15:05:49 +0530

    minor fixes, for different roles

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -15,23 +15,23 @@ import { USER_API_END_POINT } from '@/utils/constant'
 import { setUser } from "@/Redux/authSlice"
 
 const Navbar = () => {
-  const {user} = useSelector(store => store.auth)
+  const { user } = useSelector(store => store.auth)
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
   const logoutHandler = async () => {
     try {
-        const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
-        if (res.data.success) {
-            dispatch(setUser(null));
-            navigate("/");
-            toast.success(res.data.message);
-        }
+      const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+      if (res.data.success) {
+        dispatch(setUser(null));
+        navigate("/");
+        toast.success(res.data.message);
+      }
     } catch (error) {
-        console.log(error);
-        toast.error(error.response.data.message);
+      console.log(error);
+      toast.error(error.response.data.message);
     }
-}
+  }
   return (
     <div className="pt-3">
       <div className='flex items-center justify-between mx-10 lg:mx-auto max-w-7xl h-12 ' >
@@ -40,20 +40,31 @@ const Navbar = () => {
         </div>
         <div className="flex items-center gap-12">
           <ul className='font-medium items-center gap-5 hidden sm:flex'>
-            <li> <Link to='/'>Home</Link> </li>
-            <li><Link to='/jobs'>Jobs</Link> </li>
-            <li><Link to='/browse'>Browse</Link> </li>
+            {
+              user && user.role === 'recruiter' ? (
+                <>
+                  <li><Link to="/admin/companies">Companies</Link></li>
+                  <li><Link to="/admin/jobs">Jobs</Link></li>
+                </>
+              ) : (
+                <>
+                  <li><Link to="/">Home</Link></li>
+                  <li><Link to="/jobs">Jobs</Link></li>
+                  <li><Link to="/browse">Browse</Link></li>
+                </>
+              )
+            }
           </ul>
           {!user ? (
             <div className='flex items-center gap-2'>
-            <Link to="/login"><Button variant="outline">Login</Button></Link>
-            <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button></Link>
+              <Link to="/login"><Button variant="outline">Login</Button></Link>
+              <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button></Link>
             </div>
           ) : (
             <Popover>
               <PopoverTrigger asChild>
                 <Avatar className="cursor-pointer">
-                  <AvatarImage  src={user?.profile?.profilePhoto || "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"} alt="@shadcn" className=" size-10 rounded-full border object-cover" />
+                  <AvatarImage src={user?.profile?.profilePhoto || "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"} alt="@shadcn" className=" size-10 rounded-full border object-cover" />
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
               </PopoverTrigger>
@@ -61,7 +72,7 @@ const Navbar = () => {
               <PopoverContent className="w-80">
                 <div className=" flex items-center gap-4  ">
                   <Avatar className="cursor-pointer ">
-                    <AvatarImage  src={user?.profile?.profilePhoto || "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"} alt="@shadcn" className="size-10 rounded-full border object-cover" />
+                    <AvatarImage src={user?.profile?.profilePhoto || "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"} alt="@shadcn" className="size-10 rounded-full border object-cover" />
                   </Avatar>
                   <div>
                     <h4 className="font-medium">{user?.fullname}</h4>
@@ -69,10 +80,14 @@ const Navbar = () => {
                   </div>
                 </div>
                 <div className="flex flex-col text-gray-600 m-2">
-                  <div className="flex w-fil items-center gap-2 cursor-pointer ">
-                    <User2 className="" />
-                    <Button variant="link" className='outline-none text-gray-600'> <Link to='/profile'>View Profile </Link> </Button>
-                  </div>
+                  {
+                    user && user.role === 'student' && (
+                      <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                        <User2 />
+                        <Button variant="link"> <Link to="/profile">View Profile</Link></Button>
+                      </div>
+                    )
+                  }
                   <div className="flex w-fil items-center gap-2 cursor-pointer">
                     <LogOut className="" />
                     <Button onClick={logoutHandler} variant="link" className="text-gray-600">Logout</Button>
