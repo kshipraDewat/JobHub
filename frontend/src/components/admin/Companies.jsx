6a5041f0b3962aae57b21f4584fa5d0b73b07d5a
kshipraDewat <kshipradewat66@gmail.com>
2024-11-30 17:00:31 +0530

    Filter companies by text , Implementation

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import CompaniesTable from './CompaniesTable'
 import { useNavigate } from 'react-router-dom'
 import useGetAllCompanies from '@/hooks/useGetAllCompanies'
+import { useDispatch } from 'react-redux'
+import { setSearchCompanyByText } from '@/Redux/companySlice'
 
 const Companies = () => {
-    useGetAllCompanies()
-    const navigate = useNavigate()
+    useGetAllCompanies();
+    const [input, setInput] = useState("");
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(setSearchCompanyByText(input));
+    },[input]);
     return (
         <div>
             <Navbar />
--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -1,13 +1,25 @@
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { Edit2, MoreHorizontal } from 'lucide-react'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const CompaniesTable = () => {
-    const { companies } = useSelector(store => store.company)
+    const { companies, searchCompanyByText } = useSelector(store => store.company);
+    const [filterCompany, setFilterCompany] = useState(companies);
+    const navigate = useNavigate();
+    useEffect(() => {
+        const filteredCompany = companies.length >= 0 && companies.filter((company) => {
+            if (!searchCompanyByText) {
+                return true
+            };
+            return company?.name?.toLowerCase().includes(searchCompanyByText.toLowerCase());
+
+        });
+        setFilterCompany(filteredCompany);
+    }, [companies, searchCompanyByText])
     return (
         <div>
             <Table>
@@ -22,7 +34,7 @@ const CompaniesTable = () => {
                 </TableHeader>
                 <TableBody>
                     {
-                        companies?.map((company) => (
+                        filterCompany?.map((company) => (
                             <tr>
                                 <TableCell>
                                     <Avatar>
